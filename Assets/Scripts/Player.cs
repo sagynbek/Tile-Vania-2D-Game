144253ba7b3c453235eff5686d961510e2a1c69e
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-19 16:41:36 +0300

    Player jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,22 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +29,16 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if( CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void Run()
@@ -28,13 +46,18 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool isPlayerRunning = Mathf.Abs(playerVelocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", isPlayerRunning);
     }
+
+
 
     private void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        bool isPlayerRunning = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
 
-        if (playerHasHorizontalSpeed)
+        if (isPlayerRunning)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
         }
