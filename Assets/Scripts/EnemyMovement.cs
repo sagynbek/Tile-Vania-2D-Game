cf6b8b0f4362bce03818ddab1672ac80b0f5cb0d
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-19 23:55:54 +0300

    Moving enemy added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D myRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(transform.localScale.x), transform.localScale.y);
+    }
+}
