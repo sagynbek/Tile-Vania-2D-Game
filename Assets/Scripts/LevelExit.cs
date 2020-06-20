04158584c347810fa6859cb542fe9d6478b49203
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-06-20 14:13:21 +0300

    Start Menu and Level exit gate done

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] int waitInSeconds = 2;
+    [SerializeField] float levelExitSlowMotionFactor = 0.2f;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>()) { 
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        Time.timeScale = levelExitSlowMotionFactor;
+        yield return new WaitForSecondsRealtime(waitInSeconds);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
